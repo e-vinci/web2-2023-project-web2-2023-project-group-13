752465a9ff8f8a784c7c8faa8b223f4f0d45b422
Erica <erica.barros@student.vinci.be>
2023-11-13 15:48:22 +0100

    added the CSS for the Homepage buttons and added the buttons in the homepage

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,38 @@
 const HomePage = () => {
   const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+
+  // Create a button element
+    // Play button
+  const playButton = document.createElement('button');
+  playButton.textContent= 'PLAY';
+  playButton.className = 'retro-btn';
+
+  // Help button
+  const helpButton = document.createElement('button');
+  helpButton.textContent= 'HELP';
+  helpButton.className = 'retro-btn';
+
+  // High Scores button
+  const scoresButton = document.createElement('button');
+  scoresButton.textContent= 'HIGH SCORES';
+  scoresButton.className = 'retro-btn';
+
+  // Connect button
+  const connectButton = document.createElement('button');
+  connectButton.textContent= 'CONNECT';
+  connectButton.className = 'retro-btn';
+
+  // Sign in button
+  const signInButton = document.createElement('button');
+  signInButton.textContent= 'SIGN IN';
+  signInButton.className = 'retro-btn';
+
+  // Append the button to the main element
+  main.appendChild(playButton);
+  main.appendChild(helpButton);
+  main.appendChild(scoresButton);
+  main.appendChild(connectButton);
+  main.appendChild(signInButton);
 };
 
 export default HomePage;
