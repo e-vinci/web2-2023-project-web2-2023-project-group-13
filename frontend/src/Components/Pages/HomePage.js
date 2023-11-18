eac853935dc0fc9efc0d61ddcf940f703eaa2010
Erica <erica.barros@student.vinci.be>
2023-11-18 19:45:02 +0100

    changed: CSS for buttons and images in homepage && finished the homepage && added a few new pages: HighScorePage; LoginPage; SignInPage

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,38 +1,93 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { clearPage }  from '../../utils/render';
+// import navigation
+import Navigate from '../Router/Navigate';
+// import images
+import sky from '../../assets/sky.png';
+import sun from '../../assets/sun.png';
+import ground from '../../assets/ground.png';
+import catio from '../../assets/Catio.png';
+import cloud01 from '../../assets/cloud1.png';
+import cloud02 from '../../assets/cloud2.png';
+import cloud03 from '../../assets/cloud3.png';
+import cloud04 from '../../assets/cloud4.png';
+
 const HomePage = () => {
+  clearPage();
+  // get main
   const main = document.querySelector('main');
+  // add menu to main
+  main.innerHTML += renderMenu();
+  // get buttons
+  const playButton=document.querySelector('#playButton');
+  const highscoreButton=document.querySelector('#scoresButton');
+  const loginButton=document.querySelector('#loginButton');
+  const signinButton=document.querySelector('#signinButton');
 
-  // Create a button element
-    // Play button
-  const playButton = document.createElement('button');
-  playButton.textContent= 'PLAY';
-  playButton.className = 'retro-btn';
-
-  // Help button
-  const helpButton = document.createElement('button');
-  helpButton.textContent= 'HELP';
-  helpButton.className = 'retro-btn';
-
-  // High Scores button
-  const scoresButton = document.createElement('button');
-  scoresButton.textContent= 'HIGH SCORES';
-  scoresButton.className = 'retro-btn';
-
-  // Connect button
-  const connectButton = document.createElement('button');
-  connectButton.textContent= 'CONNECT';
-  connectButton.className = 'retro-btn';
-
-  // Sign in button
-  const signInButton = document.createElement('button');
-  signInButton.textContent= 'SIGN IN';
-  signInButton.className = 'retro-btn';
-
-  // Append the button to the main element
-  main.appendChild(playButton);
-  main.appendChild(helpButton);
-  main.appendChild(scoresButton);
-  main.appendChild(connectButton);
-  main.appendChild(signInButton);
+  // add eventListener to the buttons
+  playButton?.addEventListener('click',startGame);
+  highscoreButton?.addEventListener('click',redirectToHighScore);
+  loginButton?.addEventListener('click',redirectToLogin);
+  signinButton?.addEventListener('click',redirectToSignIn);
 };
+
+function renderMenu(){
+
+  const menu = `
+    <div id="skyContainer">
+      <img id="skyImage" src="${sky}">
+      
+      <div id="groundContainer">
+        <img id="groundImage" src="${ground}">
+      </div>
+      <div id="catioContainer">
+        <img id="catioImage" src="${catio}">
+      </div>
+      <div id="cloud02Container">
+        <img id="cloud02Image" src="${cloud02}">
+      </div>
+      <div id="cloud01Container">
+        <img id="cloud01Image" src="${cloud01}">
+      </div>
+      <div id="cloud03Container">
+        <img id="cloud03Image" src="${cloud03}">
+      </div>
+      <div id="sunContainer">
+        <img id="sunImage" src="${sun}">
+      </div>
+      <div id="cloud04Container">
+        <img id="cloud04Image" src="${cloud04}">
+      </div>
+      <div id="buttonContainer">
+        <button id="playButton" class="retro-btn">PLAY</button>
+        <br>
+        <button id="scoresButton" class="retro-btn">HIGH SCORES</button>
+        <br>
+        <button id="loginButton" class="retro-btn">LOG IN</button>
+        <br>
+        <button id="signinButton" class="retro-btn">SIGN IN</button>
+      </div>
+    </div>
+  `;
+  return menu;
+}
+
+// EventListener for every button
+
+function startGame(){
+  Navigate('/game');
+}
+
+function redirectToLogin(){
+  Navigate('/login');
+}
+
+function redirectToHighScore(){
+  Navigate('/highscore');
+}
+
+function redirectToSignIn(){
+  Navigate('/signIn');
+}
 
 export default HomePage;
--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,18 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
+import LoginPage from '../Pages/LoginPage';
+import HighScorePage from '../Pages/HighScorePage';
+import SignInPage from '../Pages/SignInPage';
 
 const routes = {
   '/': HomePage,
   '/game': GamePage,
   '/new': NewPage,
+  '/login':LoginPage,
+  '/highscore':HighScorePage,
+  '/signIn':SignInPage,
+
 };
 
 export default routes;
