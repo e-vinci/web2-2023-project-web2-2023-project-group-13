dad396662e2c142b7e257199aaa99faa2fde161f
Azmar Nisrine <nisrine.azmar@student.vinci.be>
2023-11-20 13:52:38 +0100

    add function showInstructions + pictures for the help pop up

--- frontend/src/Components/Game/GameScene.js
+++ frontend/src/Components/Game/GameScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import cadreAsset from '../../assets/cadre.png';
 import ScoreLabel from './ScoreLabel';
 import BombSpawner from './BombSpawner';
 import skyAsset from '../../assets/sky.png';
@@ -6,11 +7,18 @@ import platformAsset from '../../assets/platform.png';
 import starAsset from '../../assets/star.png';
 import bombAsset from '../../assets/bomb.png';
 import dudeAsset from '../../assets/dude.png';
+import buissonAsset from '../../assets/buisson.png';
+import flecheAsset from '../../assets/fleche_haut.png';
+import poissonAsset from '../../assets/poisson.png';
+import catAsset from '../../assets/catio.png';
+
 
 const GROUND_KEY = 'ground';
 const DUDE_KEY = 'dude';
 const STAR_KEY = 'star';
 const BOMB_KEY = 'bomb';
+const CADRE_KEY = 'cadre';
+
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -28,6 +36,11 @@ class GameScene extends Phaser.Scene {
     this.load.image(GROUND_KEY, platformAsset);
     this.load.image(STAR_KEY, starAsset);
     this.load.image(BOMB_KEY, bombAsset);
+    this.load.image(CADRE_KEY,cadreAsset);
+    this.load.image('poisson', poissonAsset);
+  this.load.image('buisson', buissonAsset);
+  this.load.image('fleche', flecheAsset);
+  this.load.image('chat',catAsset);
 
     this.load.spritesheet(DUDE_KEY, dudeAsset, {
       frameWidth: 32,
@@ -37,6 +50,7 @@ class GameScene extends Phaser.Scene {
 
   create() {
     this.add.image(400, 300, 'sky');
+    this.showInstructions();
     const platforms = this.createPlatforms();
     this.player = this.createPlayer();
     this.stars = this.createStars();
@@ -53,6 +67,73 @@ class GameScene extends Phaser.Scene {
     /* The Collider takes two objects and tests for collision and performs separation against them.
     Note that we could call a callback in case of collision... */
   }
+
+
+  showInstructions() {
+    // Ajoutez le cadre en premier plan
+    const cadre = this.add.sprite(400, 300, CADRE_KEY);
+  
+    // Ajustez la largeur et la hauteur du cadre ici si nécessaire
+    const scaleX = 800 / cadre.width;
+    const scaleY = 600 / cadre.height;
+    cadre.setScale(scaleX, scaleY);
+  
+   // Ajoutez les images au cadre
+  const poisson = this.add.image(cadre.x - cadre.displayWidth / 2 + 150, cadre.y - cadre.displayHeight / 2 + 150, 'poisson');
+  const buisson = this.add.image(poisson.x + poisson.displayWidth + 70, cadre.y - cadre.displayHeight / 2 + 150, 'buisson');
+  const fleche = this.add.image(buisson.x + buisson.displayWidth + 70, cadre.y - cadre.displayHeight / 2 + 150, 'fleche');
+  const chat = this.add.image(fleche.x + fleche.displayWidth + 90, cadre.y - cadre.displayHeight / 2 + 150, 'chat');
+    // Ajoutez le texte pour chaque image
+    const style = { font: '20px Arial', fill: '#000' };
+    const poissonText =this.add.text(poisson.x, poisson.y + poisson.displayHeight / 2 + 10, 'Points', style).setOrigin(0.5);
+    const buissonText=this.add.text(buisson.x, buisson.y + buisson.displayHeight / 2 + 10, 'Danger', style).setOrigin(0.5);
+    const flecheText=this.add.text(fleche.x, fleche.y + fleche.displayHeight / 2 + 15, 'Jump', style).setOrigin(0.5);
+    const chatText =this.add.text(chat.x, chat.y + chat.displayHeight / 2 + 10, 'Main Character', style).setOrigin(0.5);
+  
+    // Ajoutez le texte dans le cadre
+    const instructionsText = `
+    Welcome to Catio!
+
+    Instructions:
+    - Press the up arrow key to make the cat jump.
+    - Collect as many fish as possible to score points.
+    - Avoid obstacles and try to stay alive as long as you can.
+    
+    Have a good time!
+    
+    `;
+  
+    // Calculez la position du texte en fonction de la position du cadre
+    const textX = cadre.x - cadre.displayWidth / 2 + 70;
+    const textY = cadre.y - cadre.displayHeight / 2 + 200; // déplace le texte plus bas
+  
+    const text = this.add.text(textX, textY, instructionsText, style);
+  
+    // Ajoutez un bouton "OK" dans le cadre
+    const okButton = this.add.text(cadre.x, text.y + text.displayHeight +30, 'OK', { fontSize: '24px', fill: '#000', backgroundColor: '#ddd', padding: { x: 10, y: 5 } })
+      .setOrigin(0.5)
+      .setInteractive(); // Permet d'ajouter des événements interactifs
+  
+    // Ajoutez un gestionnaire d'événements pour le bouton "OK"
+    okButton.on('pointerdown', () => {
+      cadre.destroy();
+      text.destroy();
+      okButton.destroy();
+      poisson.destroy();
+      buisson.destroy();
+      fleche.destroy();
+      chat.destroy();
+      poissonText.destroy();
+      buissonText.destroy();
+      chatText.destroy();
+      poissonText.destroy();
+      flecheText.destroy();
+
+    });
+  }
+  
+  
+
 
   update() {
     if (this.gameOver) {
--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,27 @@
+
+import Navigate from '../Router/Navigate';
+// eslint-disable-next-line no-unused-vars
+import GameScene from '../Game/GameScene';
+
+
 const HomePage = () => {
   const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+
+  // Ajoutez le contenu de votre page d'accueil
+  main.innerHTML = `
+    <p>Deal with the content of your HomePage</p>
+    <button id="helpButton">Help</button>
+  `;
+
+  // Ajoutez un gestionnaire d'événements pour le bouton Help
+  const helpButton = document.getElementById('helpButton');
+  helpButton.addEventListener('click', showInstructions);
+
+  // Fonction pour afficher les instructions
+  function showInstructions() {
+    // Naviguez vers la scène du jeu pour afficher les instructions
+    Navigate('/game');
+  }
 };
 
 export default HomePage;
