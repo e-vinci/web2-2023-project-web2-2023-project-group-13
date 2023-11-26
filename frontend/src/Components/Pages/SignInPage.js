deb381f787cf9b0260ae2addb7cea5a158ad1b5a
Topet <bastien.topet@student.vinci.be>
2023-11-26 11:24:37 +0100

    background signIn

--- frontend/src/Components/Pages/SignInPage.js
+++ frontend/src/Components/Pages/SignInPage.js
@@ -1,6 +1,15 @@
 import { clearPage, renderPageTitle } from '../../utils/render';
 import Navigate from '../Router/Navigate';
 
+import sky from '../../assets/sky.png';
+import sun from '../../assets/sun.png';
+import ground from '../../assets/longGround.png';
+import catio from '../../assets/Catio.png';
+import cloud01 from '../../assets/cloud1.png';
+import cloud02 from '../../assets/cloud2.png';
+import cloud03 from '../../assets/cloud3.png';
+import cloud04 from '../../assets/cloud4.png';
+
 const SignInPage = () => {
   clearPage();
   renderPageTitle('Sign In');
@@ -11,13 +20,11 @@ const SignInPage = () => {
 function renderForm(){
     const main = document.querySelector('main');
     const form = document.createElement('form');
-    form.className = 'form';
     const firstname = document.createElement('input');
     firstname.type = 'text';
     firstname.id = 'firstname';
     firstname.placeholder = 'firstname';
     firstname.required = true;
-    firstname.className = 'input';
     const lastname = document.createElement('input');
     lastname.type = 'text';
     lastname.id = 'lastname';
@@ -40,7 +47,6 @@ function renderForm(){
     const submit = document.createElement('input');
     submit.value = 'Sign In';
     submit.type = 'submit';
-    submit.className = 'buttonSubmit';
     form.appendChild(firstname);
     form.appendChild(lastname);
     form.appendChild(email);
@@ -49,6 +55,7 @@ function renderForm(){
     form.appendChild(submit);
     main.appendChild(form);
     form.addEventListener('submit', addUser);
+    main.innerHTML += renderMenu();
   }
 
   async function addUser(u) {
@@ -84,5 +91,34 @@ function renderForm(){
     console.log('User : ', newUser);
 
     Navigate('/');
+  }
+
+function renderMenu(){
+  const menu = `
+    <div id="skyContainer">
+    <img id="skyImage" src="${sky}">
+    <div id="groundContainer">
+      <img id="groundImage" src="${ground}">
+    </div>
+    <div id="catioContainer">
+      <img id="catioImage" src="${catio}">
+    </div>
+    <div id="cloud02Container">
+      <img id="cloud02Image" src="${cloud02}">
+    </div>
+    <div id="cloud01Container">
+      <img id="cloud01Image" src="${cloud01}">
+    </div>
+    <div id="cloud03Container">
+      <img id="cloud03Image" src="${cloud03}">
+    </div>
+    <div id="sunContainer">
+      <img id="sunImage" src="${sun}">
+    </div>
+    <div id="cloud04Container">
+      <img id="cloud04Image" src="${cloud04}">
+    </div>
+    `
+    return menu;
   }
   export default SignInPage;
