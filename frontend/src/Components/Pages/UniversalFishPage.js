5ede411e1c1dc2add00a2eacaad2f69da47c5810
Topet <bastien.topet@student.vinci.be>
2023-12-15 22:12:36 +0100

    add email in UniversalScoresFish

--- frontend/src/Components/Pages/UniversalFishPage.js
+++ frontend/src/Components/Pages/UniversalFishPage.js
@@ -18,7 +18,7 @@ const UniversalFishPage = async () => {
      <h1>My High Scores By Fish</h1>
      ${table.map((score) => `
    <div class="timer">
-      ${score.fish} fish
+      ${score.email} ${score.fish} fish
    </div>
  `).join('')}
    </div>
